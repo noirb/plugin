0255cc006b38a3e246dc74e52d0158b306cc8a99
tome-yamada <tome-yamada@nii.ac.jp>
2016-03-08 19:02:16 +0900

    Make Perception Neuron controller.

---
+++ sample_controllers/ManNiiAvatarControllerByPerceptionNeuron/ManNiiAvatarControllerByPerceptionNeuron.cpp
@@ -0,0 +1,127 @@
+/*
+ * ManNiiAvatarControllerByPerceptionNeuron.cpp
+ *
+ *  Created on: 2016/03/02
+ *      Author: Yamada
+ */
+
+#include "ManNiiAvatarControllerByPerceptionNeuron.h"
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/exception/diagnostic_information.hpp>
+#include <sigverse/plugin/common/sensor/SensorData.h>
+#include <cmath>
+
+///@brief Initialize this controller.
+void ManNiiAvatarControllerByPerceptionNeuron::onInit(InitEvent &evt)
+{
+	this->readIniFileAndInitialize();
+
+	SimObj *myself = getObj(myname());
+
+	this->perceptionNeuronDeviceManager.initPositionAndRotation(myself);
+}
+
+
+///@brief Movement of the robot.
+double ManNiiAvatarControllerByPerceptionNeuron::onAction(ActionEvent &evt)
+{
+	bool perceptionNeuronAvailable = checkService(this->perceptionNeuronDeviceManager.serviceName);
+
+	if (perceptionNeuronAvailable && this->perceptionNeuronDeviceManager.service == NULL)
+	{
+		this->perceptionNeuronDeviceManager.service = connectToService(this->perceptionNeuronDeviceManager.serviceName);
+	}
+	else if (!perceptionNeuronAvailable && this->perceptionNeuronDeviceManager.service != NULL)
+	{
+		this->perceptionNeuronDeviceManager.service = NULL;
+	}
+
+	return 1.0;
+}
+
+void ManNiiAvatarControllerByPerceptionNeuron::onRecvMsg(RecvMsgEvent &evt)
+{
+	try
+	{
+		std::string allMsg = evt.getMsg();
+
+//		std::cout << "msg:" << allMsg << std::endl;
+
+		// Decode message to sensor data of Perception Neuron.
+		std::map<std::string, std::vector<std::string> > sensorDataMap = PerceptionNeuronSensorData::decodeSensorData(allMsg);
+
+		if (sensorDataMap.find(MSG_KEY_DEV_TYPE) == sensorDataMap.end()){ return; }
+		if(sensorDataMap[MSG_KEY_DEV_TYPE][0]     !=this->perceptionNeuronDeviceManager.deviceType    ){ return; }
+		if(sensorDataMap[MSG_KEY_DEV_UNIQUE_ID][0]!=this->perceptionNeuronDeviceManager.deviceUniqueID){ return; }
+
+		PerceptionNeuronSensorData sensorData;
+
+		sensorData.setSensorData(sensorDataMap);
+
+		ManBvhPosture posture = PerceptionNeuronDeviceManager::convertSensorData2ManBvhPosture(sensorData);
+
+		// Set SIGVerse quaternions and positions
+		SimObj *obj = getObj(myname());
+		this->perceptionNeuronDeviceManager.setPosture2ManBvh(obj, posture);
+	}
+	catch(SimObj::Exception &err)
+	{
+		LOG_MSG(("Exception: %s", err.msg()));
+	}
+	catch(std::string &err)
+	{
+		LOG_MSG(("Exception: %s", err.c_str()));
+	}
+	catch(...)
+	{
+		std::cout << "some error occurred." << std::endl;
+	}
+}
+
+
+
+
+///@brief Read parameter file.
+///@return When couldn't read parameter file, return false;
+void ManNiiAvatarControllerByPerceptionNeuron::readIniFileAndInitialize()
+{
+	try
+	{
+		std::ifstream ifs(parameterFileName.c_str());
+
+		// Parameter file is "not" exists.
+		if (ifs.fail())
+		{
+			std::cout << "Not exist : " << parameterFileName << std::endl;
+			exit(-1);
+		}
+
+		// Parameter file is exists.
+		std::cout << "Read " << parameterFileName << std::endl;
+		boost::property_tree::ptree pt;
+		boost::property_tree::read_ini(parameterFileName, pt);
+
+		std::string serviceName    = pt.get<std::string>(PARAMETER_FILE_KEY_GENERAL_SERVICE_NAME);
+		std::string deviceType     = pt.get<std::string>(PARAMETER_FILE_KEY_GENERAL_DEVICE_TYPE);
+		std::string deviceUniqueID = pt.get<std::string>(PARAMETER_FILE_KEY_GENERAL_DEVICE_UNIQUE_ID);
+
+		std::cout << PARAMETER_FILE_KEY_GENERAL_SERVICE_NAME     << ":" << serviceName    << std::endl;
+		std::cout << PARAMETER_FILE_KEY_GENERAL_DEVICE_TYPE      << ":" << deviceType     << std::endl;
+		std::cout << PARAMETER_FILE_KEY_GENERAL_DEVICE_UNIQUE_ID << ":" << deviceUniqueID << std::endl;
+
+		this->perceptionNeuronDeviceManager = PerceptionNeuronDeviceManager(serviceName, deviceType, deviceUniqueID);
+	}
+	catch (boost::exception &ex)
+	{
+		std::cout << parameterFileName << " ERR :" << *boost::diagnostic_information_what(ex) << std::endl;
+	}
+}
+
+
+extern "C" Controller * createController()
+{
+	return new ManNiiAvatarControllerByPerceptionNeuron;
+}
+
---
+++ sample_controllers/ManNiiAvatarControllerByPerceptionNeuron/ManNiiAvatarControllerByPerceptionNeuron.h
@@ -0,0 +1,42 @@
+/*
+ * ManNiiAvatarControllerByPerceptionNeuron.h
+ *
+ *  Created on: 2016/03/02
+ *      Author: Yamada
+ */
+#ifndef SIGVERSE_MAN_NII_AVATAR_CONTROLLER_BY_PERCEPTION_NEURON_H
+#define SIGVERSE_MAN_NII_AVATAR_CONTROLLER_BY_PERCEPTION_NEURON_H
+
+#include <sigverse/commonlib/Controller.h>
+#include <sigverse/commonlib/ControllerEvent.h>
+
+#include <sigverse/plugin/controller/common/ManNiiAvatarController.h>
+#include <sigverse/plugin/controller/common/ManBvhPosture.h>
+#include <sigverse/plugin/controller/common/sensor/PerceptionNeuronDeviceManager.h>
+#include <sigverse/plugin/common/sensor/PerceptionNeuronSensorData.h>
+
+
+class ManNiiAvatarControllerByPerceptionNeuron : public ManNiiAvatarController
+{
+public:
+	///@brief Parameter file name.
+	static const std::string parameterFileName;
+
+	///@brief Movement of the robot.
+	double onAction(ActionEvent &evt);
+
+	///@brief Message heard by the robot.
+	void onRecvMsg(RecvMsgEvent &evt);
+
+	///@brief Initialize this controller.
+	void onInit(InitEvent &evt);
+
+	void readIniFileAndInitialize();
+
+	PerceptionNeuronDeviceManager perceptionNeuronDeviceManager;
+};
+
+///@brief Parameter file name.
+const std::string ManNiiAvatarControllerByPerceptionNeuron::parameterFileName = "PerceptionNeuron.ini";
+
+#endif //SIGVERSE_MAN_NII_AVATAR_CONTROLLER_BY_PERCEPTION_NEURON_H
