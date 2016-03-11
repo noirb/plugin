e43af096ec67d34000359d48e7095d3abe05a978
tome-yamada <tome-yamada@nii.ac.jp>
2016-03-11 17:17:46 +0900

    Move methods to the cpp files.

--- srcs/sigverse/plugin/controller/common/ManBvhPosture.cpp
+++ srcs/sigverse/plugin/controller/common/ManBvhPosture.cpp
@@ -32,3 +32,28 @@ ManBvhPosture::ManBvhPosture()
 		this->joint[i].angle.z = 0.0;
 	}
 }
+
+
+std::string ManBvhPosture::manBvhJointTypeStr(ManBvhJointType e)
+{
+	switch(e)
+	{
+		case HIP_JOINT       : { return("HIP_JOINT" ); }
+		case WAIST_JOINT     : { return("WAIST_JOINT" ); }
+		case NECK_JOINT      : { return("NECK_JOINT" ); }
+		case LSHOULDER_JOINT : { return("LSHOULDER_JOINT" ); }
+		case LELBOW_JOINT    : { return("LELBOW_JOINT" ); }
+		case LWRIST_JOINT    : { return("LWRIST_JOINT" ); }
+		case RSHOULDER_JOINT : { return("RSHOULDER_JOINT" ); }
+		case RELBOW_JOINT    : { return("RELBOW_JOINT" ); }
+		case RWRIST_JOINT    : { return("RWRIST_JOINT" ); }
+		case LHIP_JOINT      : { return("LHIP_JOINT" ); }
+		case LKNEE_JOINT     : { return("LKNEE_JOINT" ); }
+		case LANKLE_JOINT    : { return("LANKLE_JOINT" ); }
+		case RHIP_JOINT      : { return("RHIP_JOINT" ); }
+		case RKNEE_JOINT     : { return("RKNEE_JOINT" ); }
+		case RANKLE_JOINT    : { return("RANKLE_JOINT" ); }
+		//default: { return("illegal"); }
+	}
+}
+
--- srcs/sigverse/plugin/controller/common/ManNiiPosture.h
+++ srcs/sigverse/plugin/controller/common/ManNiiPosture.h
@@ -56,48 +56,6 @@ public:
 		ManNiiJointType_Count = (REYE_JOINT0 + 1)
 	};
 
-	static std::string manNiiJointTypeStr(ManNiiJointType e)
-	{
-		switch(e)
-		{
-			case HEAD_JOINT0 : { return("HEAD_JOINT0" ); }
-			case HEAD_JOINT1 : { return("HEAD_JOINT1" ); }
-			case LARM_JOINT0 : { return("LARM_JOINT0" ); }
-			case LARM_JOINT1 : { return("LARM_JOINT1" ); }
-			case LARM_JOINT2 : { return("LARM_JOINT2" ); }
-			case LARM_JOINT3 : { return("LARM_JOINT3" ); }
-			case LARM_JOINT4 : { return("LARM_JOINT4" ); }
-			case LARM_JOINT5 : { return("LARM_JOINT5" ); }
-			case LARM_JOINT6 : { return("LARM_JOINT6" ); }
-			case LARM_JOINT7 : { return("LARM_JOINT7" ); }
-			case RARM_JOINT0 : { return("RARM_JOINT0" ); }
-			case RARM_JOINT1 : { return("RARM_JOINT1" ); }
-			case RARM_JOINT2 : { return("RARM_JOINT2" ); }
-			case RARM_JOINT3 : { return("RARM_JOINT3" ); }
-			case RARM_JOINT4 : { return("RARM_JOINT4" ); }
-			case RARM_JOINT5 : { return("RARM_JOINT5" ); }
-			case RARM_JOINT6 : { return("RARM_JOINT6" ); }
-			case RARM_JOINT7 : { return("RARM_JOINT7" ); }
-			case WAIST_JOINT0: { return("WAIST_JOINT0"); }
-			case WAIST_JOINT1: { return("WAIST_JOINT1"); }
-			case WAIST_JOINT2: { return("WAIST_JOINT2"); }
-			case ROOT_JOINT0 : { return("ROOT_JOINT0" ); }
-			case ROOT_JOINT1 : { return("ROOT_JOINT1" ); }
-			case ROOT_JOINT2 : { return("ROOT_JOINT2" ); }
-			case LLEG_JOINT2 : { return("LLEG_JOINT2" ); }
-			case LLEG_JOINT4 : { return("LLEG_JOINT4" ); }
-			case LLEG_JOINT6 : { return("LLEG_JOINT6" ); }
-			case RLEG_JOINT2 : { return("RLEG_JOINT2" ); }
-			case RLEG_JOINT4 : { return("RLEG_JOINT4" ); }
-			case RLEG_JOINT6 : { return("RLEG_JOINT6" ); }
-			case LEYE_JOINT1 : { return("LEYE_JOINT1" ); }
-			case LEYE_JOINT0 : { return("LEYE_JOINT0" ); }
-			case REYE_JOINT1 : { return("REYE_JOINT1" ); }
-			case REYE_JOINT0 : { return("REYE_JOINT0" ); }
-			//default: { return("illegal"); }
-		}
-	}
-
 	//Avatar joints
 	enum SigVec
 	{
@@ -119,6 +77,8 @@ public:
 		bool isValid;
 	} ManNiiJoint;
 
+
+	static std::string manNiiJointTypeStr(ManNiiJointType e);
 
 	static SigCmn::Vector3 getSigVec(const SigVec &sigvec);
 
