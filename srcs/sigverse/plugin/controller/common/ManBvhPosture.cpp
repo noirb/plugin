6d8e053014022c1692d74d5d83fafcb73f55e540
tome-yamada <tome-yamada@nii.ac.jp>
2016-03-11 17:52:00 +0900

    Tweak.

--- srcs/sigverse/plugin/controller/common/ManBvhPosture.cpp
+++ srcs/sigverse/plugin/controller/common/ManBvhPosture.cpp
@@ -9,24 +9,9 @@
 
 ManBvhPosture::ManBvhPosture()
 {
-	this->joint[HIP_JOINT      ].jointType = HIP_JOINT;
-	this->joint[WAIST_JOINT    ].jointType = WAIST_JOINT;
-	this->joint[NECK_JOINT     ].jointType = NECK_JOINT,
-	this->joint[LSHOULDER_JOINT].jointType = LSHOULDER_JOINT;
-	this->joint[LELBOW_JOINT   ].jointType = LELBOW_JOINT;
-	this->joint[LWRIST_JOINT   ].jointType = LWRIST_JOINT;
-	this->joint[RSHOULDER_JOINT].jointType = RSHOULDER_JOINT;
-	this->joint[RELBOW_JOINT   ].jointType = RELBOW_JOINT;
-	this->joint[RWRIST_JOINT   ].jointType = RWRIST_JOINT;
-	this->joint[LHIP_JOINT     ].jointType = LHIP_JOINT;
-	this->joint[LKNEE_JOINT    ].jointType = LKNEE_JOINT;
-	this->joint[LANKLE_JOINT   ].jointType = LANKLE_JOINT;
-	this->joint[RHIP_JOINT     ].jointType = RHIP_JOINT;
-	this->joint[RKNEE_JOINT    ].jointType = RKNEE_JOINT;
-	this->joint[RANKLE_JOINT   ].jointType = RANKLE_JOINT;
-
 	for (int i = 0; i < ManBvhJointType_Count; i++)
 	{
+		this->joint[i].jointType = i;
 		this->joint[i].angle.x = 0.0;
 		this->joint[i].angle.y = 0.0;
 		this->joint[i].angle.z = 0.0;
